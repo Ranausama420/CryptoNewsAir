8d8b9f25858193781506e0816ee1d74ea5fb72c8
Ranausama420 <43530464+Ranausama420@users.noreply.github.com>
2023-05-05 16:04:27 +0500

    change

--- website/settings.py
+++ website/settings.py
@@ -76,22 +76,22 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'cryptoDB',
-        'USER': 'postgres',
-        'PASSWORD': 'xyz@123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'cryptoDB',
+#         'USER': 'postgres',
+#         'PASSWORD': 'xyz@123',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 # DATABASES = {
 #      'default': {
 #          'ENGINE': 'djongo',
